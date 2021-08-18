ba2e88bd30842eb90365dd63a5c65258d30825a0
krsdm <krsdm@ya.ru>
2021-08-18 19:34:01 +0300

    Версия 2. Исправлены замечания ментора

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,45 +2,35 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
 
 public class UserDaoJDBCImpl implements UserDao {
-
-    private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(45) NOT NULL, lastname VARCHAR(45) NOT NULL, age INT(3) NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;";
-    private static final String DROP_TABLE = "DROP TABLE IF EXISTS users;";
-    private static final String INSERT_USER = "INSERT INTO users(name, lastname, age) VALUES (?, ?, ?);";
-    private static final String DELETE_USER = "DELETE FROM users WHERE id = ?;";
-    private static final String GET_ALL_USERS = "SELECT * FROM users;";
-    private static final String DEL_ALL_USERS = "TRUNCATE TABLE users;";
+    private final Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try (Connection connection = Util.getConnection();
-             PreparedStatement statement = connection.prepareStatement(CREATE_TABLE)) {
+        try (Statement statement = connection.createStatement()) {
 
-            statement.execute();
+            statement.execute("CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(45) NOT NULL," +
+                "lastname VARCHAR(45) NOT NULL, age INT(3) NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;");
 
         } catch (SQLException throwables) {
-            System.out.println("Не удалось создать таблицу");
             throwables.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
-        try (Connection connection = Util.getConnection();
-             PreparedStatement statement = connection.prepareStatement(DROP_TABLE)) {
+        try (Statement statement = connection.createStatement()) {
 
-            statement.execute();
+            statement.execute("DROP TABLE IF EXISTS users;");
 
         } catch (SQLException throwables) {
             System.out.println("Не удалось удалить таблицу");
@@ -49,8 +39,8 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = Util.getConnection();
-             PreparedStatement statement = connection.prepareStatement(INSERT_USER)) {
+        try (PreparedStatement statement = connection.prepareStatement(
+                     "INSERT INTO users(name, lastname, age) VALUES (?, ?, ?);")) {
 
             statement.setString(1, name);
             statement.setString(2, lastName);
@@ -65,8 +55,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = Util.getConnection();
-             PreparedStatement statement = connection.prepareStatement(DELETE_USER)) {
+        try (PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?;")) {
 
             statement.setLong(1, id);
             statement.executeUpdate();
@@ -80,10 +69,9 @@ public class UserDaoJDBCImpl implements UserDao {
     public List<User> getAllUsers() {
         ArrayList<User> users = new ArrayList<>();
 
-        try (Connection connection = Util.getConnection();
-             PreparedStatement statement = connection.prepareStatement(GET_ALL_USERS)) {
+        try (Statement statement = connection.createStatement()) {
 
-            ResultSet result = statement.executeQuery();
+            ResultSet result = statement.executeQuery("SELECT * FROM users;");
             while (result.next()) {
                 users.add(new User(
                         result.getInt("id"),
@@ -102,10 +90,9 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Connection connection = Util.getConnection();
-             PreparedStatement statement = connection.prepareStatement(DEL_ALL_USERS)) {
+        try (Statement statement = connection.createStatement()) {
 
-            statement.execute();
+            statement.execute("TRUNCATE TABLE users;");
 
         } catch (SQLException throwables) {
             System.out.println("Не удалось очистить таблицу");
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,11 +8,16 @@ public class Util {
     // реализуйте настройку соеденения с БД
 
     private static final String DB_USER = "root";
-    private static final String DB_PASS = "";
+    private static final String DB_PASS = "K5639667Dimon";
     private static final String DB_NAME = "usersdb";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
     }
 }
